81cd83634091eb02e722c320b3ee6ea4710904e4
Aleksandr Bogdanov <alex@tresorg.com>
2013-12-05 13:19:19 +0100

    Update jquery.cssGenerator.js
    fixed multiple elements
    fixed start from any type of tag

--- jquery.cssGenerator.js
+++ jquery.cssGenerator.js
@@ -1,51 +1,53 @@
-
 (function( jQuery ){
         jQuery.fn.cssGenerator = function(options) {
             base = '.';
             css = [];
-            
+
             var settings = jQuery.extend({
                 //setting this to true will use an id embed of a class, if the element has one
-                prefer_ids_over_class : false
+                prefer_ids_over_class : false,
+                callback:function(response){ console.log(response); },
+                start_from_element:false
             }, options );
 
-                
+            		get_selector = function(element){
+            			new_base = '';
+            			if( element.attr('class') || element.attr('id') ) {
+                    if( element.attr('id') && settings.prefer_ids_over_class) {
+                        new_base = '#' + element.attr('id')+' ';
+                    }else{
+                        new_base = '.' + element.attr('class').split(' ').join('.')+' ';
+                    }
+                  }else{
+                    if( typeof element.context == 'object' || typeof element[0].tagName != 'undefined' ){
+                        new_base = element[0].tagName.toLowerCase()+' ';
+                    }
+                  }
+                  return new_base;
+            		}
+
                 iterator = function(base, element){
+                		console.log( 'children in '+ get_selector(element) + ' ' + element.children().length )
+                		jQuery.each(element.children(), function(i,v){
 
-                    for (a in element.children()) {
-                        
-                        current = jQuery(element.children()[a]);
+                			current = jQuery(v);
+                      new_base = get_selector(current);
+                      last_base = base + new_base;
+                      css.push(last_base);
 
-                        if( current.attr('class') || current.attr('id') ) {
-                            
-                            if( current.attr('id') && settings.prefer_ids_over_class) {
+                      if(typeof current.children()[0] != 'undefined' ){
+                          iterator(last_base, current);
+                      }else{
+                          if( i<element.children().length ) {
 
-                                new_base = '#' + current.attr('id')+' ';
+                          }else{
+                          	return false;
+                          }
+                      }
 
-                            }else{
-                                
-                                new_base = '.' + current.attr('class').split(' ').join('.')+' ';
+                		});
 
-                            }
-
-                        }else{
-                            if( typeof current.context == 'object' ){
-                                new_base = current[0].tagName.toLowerCase()+' ';
-                            }
-                        }
-                        
-                        last_base = base + new_base;
-                        
-                        css.push(last_base);
-
-                        if(current.children().length>0){
-                            iterator(last_base, current);
-                        }else{
-                            return false;
-                        }
-                        
-                    };
-                }   
+                }
 
                 unique = function(a) {
                     return a.reduce(function(p, c) {
@@ -54,12 +56,18 @@
                     }, []);
                 };
 
-                base += jQuery(this).attr('class').split(' ').join('.')+' ';
-                
-                iterator(base, jQuery(this) );
-                
+
+                jQuery.each(jQuery(this), function(k,v){
+                	base = get_selector(jQuery(v));
+                	css.push(base);
+                	iterator(base, jQuery(v) );
+                });
+
+
                 //your css sir||mam
-                console.log( unique(css).join("{}\n")+'{}' );
+                final_css =  unique(css).join("{}\n")+'{}';
+
+                settings.callback(final_css);
 
         };
 })( jQuery );
